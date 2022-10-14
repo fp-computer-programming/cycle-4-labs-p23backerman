fc24fe25db006b859a2d397b4b0d2a40651afe40
p23backerman <p23backerman@fairfieldprep.org>
2022-10-14 12:22:54 -0400

    lab 4-4 submission

---
+++ lab_4-4.py
@@ -0,0 +1,12 @@
+
+a = 'flibbertigibet'
+print (a.find("t"))
+b = a.find("t")
+after_variable = a[b+1:b+2]
+print(after_variable)
+c = "benjamin"
+d = str.upper(c)
+print(d)
+e = 'I wish, I wish, I was a fish.'
+f = e.split(sep = ","  ,maxsplit = 2)
+print(f)
