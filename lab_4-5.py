9c87366b359da205d55aac58b1b2f02bb8b5ee17
p23backerman <p23backerman@fairfieldprep.org>
2022-10-17 13:15:39 -0400

    4-5

---
+++ lab_4-5.py
@@ -0,0 +1,6 @@
+print('What is your first name')
+person1 = input()
+print(" what is the other persons first name?")
+person2= input()
+fs = "Hi {0} my name is {1} ".format(person1, person2)
+print(fs)
